7fb326839558ba09a175d1907ce58c74a39d39a0
Francisco Silva <franciscocrosilva@gmail.com>
2024-02-24 15:53:42 +0000

    remove non essecial headers

--- legacy_idealista_s.py
+++ legacy_idealista_s.py
@@ -8,23 +8,8 @@ print("-" * 10 + "Beggining" + "-" * 10)
 session = requests.Session()
 
 headers = {
-    'authority': 'www.idealista.pt',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cache-control': 'max-age=0',
-    'dnt': '1',
     'sec-ch-device-memory': '8',
-    'sec-ch-ua': '"Chromium";v="121", "Not A(Brand";v="99"',
-    'sec-ch-ua-arch': '"arm"',
-    'sec-ch-ua-full-version-list': '"Chromium";v="121.0.6167.160", "Not A(Brand";v="99.0.0.0"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 }
 
